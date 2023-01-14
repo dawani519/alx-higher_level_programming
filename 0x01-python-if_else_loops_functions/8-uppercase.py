90ae86ede5560b2ab3da67ce53977db3b5dbc771
dawani519 <danielamos519@gmail.com>
2023-01-14 13:11:42 +0000

    upated

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    str = "best school 98 battery street"
-    for str in range(65, 90):
-        if ord(str) >= 65 and ord(str) <= 90:
-            print("{}".format(str))
+    for c in str:
+        if ord(c) >= 65 and ord(c) <= 90:
+            print("{}".format(c))
         else:
-            print("{}".format(str), end="")
+            print("{}".format(c), end="")
