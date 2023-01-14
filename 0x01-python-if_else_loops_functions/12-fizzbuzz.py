69989c4b4ebbd25d7450d0d85e586c2f96a7cc7e
dawani519 <danielamos519@gmail.com>
2023-01-14 14:02:16 +0000

    updated

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for number in range(0, 101):
+        if number = number % 3:
+            print("{Fizz}".format(number))
+            if number number % 5:
+                print("{Buzz}".format(number))
+            elif number = number % 3 and number = number % 5:
+                print("{FizzBuzz}".format(number))
+            else:
+                print("{}".format(number), end="")
+
