74b9f1cac5f6eb2ef29a0b5d5d637c3d59fec565
dawani519 <danielamos519@gmail.com>
2023-01-16 00:23:39 +0000

    updated

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     for letter in range(97, 123):
-        if chr(letter) != 'q' and chr(letter) != 'e':
-            print("{}".format(chr(letter)), end="")
+        n = []
+        if chr(letter) != n:
+            print("{[]}".format(chr(letter)), end="")
