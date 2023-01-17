3cb63da1fe4141b87810f1f15340fdadfdfd14bf
dawani519 <danielamos519@gmail.com>
2023-01-17 15:21:10 +0000

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-
-    if len(argv) == 0:
-        print("Number of argument(s): 0.")
-    elif len(argv) == 1:
-        print("Number of argument: 1.")
-        print(f"1: {argv[0]}")
-    else:
-        print(f"Number of arguments: {len(argv)}")
-        for index, arg in enumerate(argv):
-            print(f"{index+1}: {arg}")
+    for i in len(argv):
+        print("{}: {}".format(i, len(argv))
